3b16a4d53472376f0b212227afc5b7c22b17ee9b
KALININ ALEXEY <2257855@gmail.com>
2021-03-11 04:59:22 +0300

    create: addaptive for mobile devices. jsyandex map

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [7.8939, 98.2992],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 15,
+  });
+
+  // Хочу поставить Метку <<<<<<<<<
+  var placeMark = new ymaps.PlaceMark([7.895, 98.3002]);
+  map.geoObjects.add(placeMark);
+}
+
 const swiper = new Swiper(".swiper-container", {
   loop: true,
   navigation: {
@@ -8,5 +29,4 @@ const swiper = new Swiper(".swiper-container", {
     enabled: true,
     onlyInViewport: true,
   },
-
 });
