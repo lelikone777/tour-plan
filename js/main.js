c8f8751d8214cc7560695421efb166290b4405fb
KALININ ALEXEY <2257855@gmail.com>
2021-03-24 20:18:39 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -1,59 +1,78 @@
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-ymaps.ready(init);
-function init() {
-  // Создание карты.
-  var myMap = new ymaps.Map("myMap", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [7.8939, 98.2992],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 15,
+$(document).ready(function () {
+  // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  ymaps.ready(init);
+  function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("myMap", {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [7.8939, 98.2992],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 15,
+    });
+
+    // Хочу поставить Метку <<<<<<<<<
+    var placemark = new ymaps.Placemark([7.895, 98.3002], {
+      // baloonContentHeader: "sdgsg",
+      baloonContentBody: "GRAND HILTON HOTEL",
+      hintContent: "GRAND HILTON HOTEL",
+    });
+    myMap.geoObjects.add(placemark);
+  }
+
+  const hotelSlider = new Swiper(".hotel-slider", {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      arrowLeftRight: true,
+    },
+    // effect: 'flip',
   });
 
-  // Хочу поставить Метку <<<<<<<<<
-  var placemark = new ymaps.Placemark([7.895, 98.3002], {
-    // baloonContentHeader: "sdgsg",
-    baloonContentBody: "GRAND HILTON HOTEL",
-    hintContent: "GRAND HILTON HOTEL",
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    loop: true,
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
   });
-  myMap.geoObjects.add(placemark);
-}
 
-const hotelSlider = new Swiper(".hotel-slider", {
-  loop: true,
-  autoplay: {
-    delay: 5000,
-  },
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    arrowLeftRight: true,
-  },
-  // effect: 'flip',
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("клик по кнопке меню");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+    document.body.classList.toggle("lock");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass('modal__overlay--visible');
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
-
-const reviewsSlider = new Swiper(".reviews-slider", {
-  loop: true,
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-});
-
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
-  document.body.classList.toggle('lock');  
-});
-
