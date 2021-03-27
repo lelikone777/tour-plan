47c6c363fed03cd86868d3d96a878287007842be
KALININ ALEXEY <2257855@gmail.com>
2021-03-27 17:01:22 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 $(document).ready(function () {
-  // Функция ymaps.ready() будет вызвана, когда
-  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-  ymaps.ready(init);
-  function init() {
-    // Создание карты.
-    var myMap = new ymaps.Map("myMap", {
-      // Координаты центра карты.
-      // Порядок по умолчанию: «широта, долгота».
-      // Чтобы не определять координаты центра карты вручную,
-      // воспользуйтесь инструментом Определение координат.
-      center: [7.8939, 98.2992],
-      // Уровень масштабирования. Допустимые значения:
-      // от 0 (весь мир) до 19.
-      zoom: 15,
-    });
+  // // Функция ymaps.ready() будет вызвана, когда
+  // // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  // ymaps.ready(init);
+  // function init() {
+  //   // Создание карты.
+  //   var myMap = new ymaps.Map("myMap", {
+  //     // Координаты центра карты.
+  //     // Порядок по умолчанию: «широта, долгота».
+  //     // Чтобы не определять координаты центра карты вручную,
+  //     // воспользуйтесь инструментом Определение координат.
+  //     center: [7.8939, 98.2992],
+  //     // Уровень масштабирования. Допустимые значения:
+  //     // от 0 (весь мир) до 19.
+  //     zoom: 15,
+  //   });
 
-    // Хочу поставить Метку <<<<<<<<<
-    var placemark = new ymaps.Placemark([7.895, 98.3002], {
-      // baloonContentHeader: "sdgsg",
-      baloonContentBody: "GRAND HILTON HOTEL",
-      hintContent: "GRAND HILTON HOTEL",
-    });
-    myMap.geoObjects.add(placemark);
-  }
+  //   // Хочу поставить Метку <<<<<<<<<
+  //   var placemark = new ymaps.Placemark([7.895, 98.3002], {
+  //     // baloonContentHeader: "sdgsg",
+  //     baloonContentBody: "GRAND HILTON HOTEL",
+  //     hintContent: "GRAND HILTON HOTEL",
+  //   });
+  //   myMap.geoObjects.add(placemark);
+  // }
+
+
+  
 
   const hotelSlider = new Swiper(".hotel-slider", {
     loop: true,
