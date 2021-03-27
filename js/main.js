87755a27d30f38e46df25189e7d533f7eb75e035
KALININ ALEXEY <2257855@gmail.com>
2021-03-27 14:31:05 +0300

    1

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ $(document).ready(function () {
 
   var menuButton = document.querySelector(".menu-button");
   menuButton.addEventListener("click", function () {
-    console.log("клик по кнопке меню");
     document
       .querySelector(".navbar-bottom")
       .classList.toggle("navbar-bottom--visible");
